6e5ed162caa63a7801d61946729f4f23baba9430
ArchLeaders <archleadership28@gmail.com>
2024-04-19 00:15:11 -0700

    Implement the software

---
+++ src/MsbtToYaml/ArgumentExtension.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace MsbtToYaml;
+
+public static class ArgumentExtension
+{
+    public static bool TryGetOutput(this string[] args, ref int index, [MaybeNullWhen(false)] out string output)
+    {
+        if (++index >= args.Length - 1) {
+            output = default;
+            return false;
+        }
+
+        output = args[index] switch {
+            "-o" or "--output" => args[++index],
+            _ => null
+        };
+
+        return output is not null;
+    }
+}
--- src/MsbtToYaml/Program.cs
+++ src/MsbtToYaml/Program.cs
@@ -1,10 +1,56 @@
-﻿using System.Reflection;
+﻿using CommunityToolkit.HighPerformance.Buffers;
+using MessageStudio.Formats.BinaryText;
+using MsbtToYaml;
+using Revrs.Extensions;
+using System.Reflection;
+using System.Text;
+
+const ulong MSBT_MAGIC = 0x6E4264745367734DUL;
 
 Console.WriteLine($"""
     MSBT to YAML [Version {Assembly.GetExecutingAssembly().GetName().Version?.ToString(3) ?? "???"}]
-    (c) ArchLeaders. MIT.
+    (c) 2024 ArchLeaders. MIT.
+
     """);
 
-foreach (string arg in args) {
-    
+if (args.Length == 0 || args[0].ToLower() is "h" or "help" or "-h" or "--help") {
+    Console.WriteLine("""
+          Usage:
+        <input> [-o|--output OUTPUT] ...
+
+          Documentation:
+        github.com/ArchLeaders/MsbtToYaml
+        """);
+}
+
+for (int i = 0; i < args.Length; i++) {
+    string input = args[i];
+
+    using FileStream fs = File.OpenRead(input);
+    int size = Convert.ToInt32(fs.Length);
+    using SpanOwner<byte> buffer = SpanOwner<byte>.Allocate(size);
+    fs.Read(buffer.Span);
+    fs.Dispose();
+
+    bool isMsbt = buffer.Span.Read<ulong>() == MSBT_MAGIC;
+
+    if (!args.TryGetOutput(ref i, out string? output)) {
+        output = Path.ChangeExtension(input, isMsbt switch {
+            true => "yaml",
+            false => "msbt"
+        });
+    }
+
+    if (isMsbt) {
+        Msbt msbt = Msbt.FromBinary(buffer.Span);
+        string yaml = msbt.ToYaml();
+        File.WriteAllText(output, yaml);
+    }
+    else {
+        using SpanOwner<char> yaml = SpanOwner<char>.Allocate(size);
+        Encoding.UTF8.GetChars(buffer.Span, yaml.Span);
+        Msbt msbt = Msbt.FromYaml(yaml.Span);
+        using FileStream outputFileStream = File.Create(output);
+        msbt.WriteBinary(outputFileStream);
+    }
 }
